13e4e89cc0b45df1eb83286548aba9a432c57871
Pat Hickey <pat@galois.com>
2013-06-06 09:41:54 -0700

    ivory-tower: add stateProxy

--- ivory-tower/src/Ivory/Tower.hs
+++ ivory-tower/src/Ivory/Tower.hs
@@ -58,6 +58,9 @@ module Ivory.Tower
   , Name
   , Assembly()
   , Schedule
+
+  -- Built in helpers
+  , stateProxy
   ) where
 
 import Ivory.Tower.Channel
@@ -66,4 +69,18 @@ import Ivory.Tower.EventLoop
 import Ivory.Tower.Tower
 import Ivory.Tower.Task
 import Ivory.Tower.Types
+
+import Ivory.Language
+
+stateProxy :: (IvoryType area, IvoryZero area)
+           => ChannelSink area -> Tower (DataSink area)
+stateProxy chsink = do
+  (src_data, snk_data) <- dataport
+  task "stateProxy" $ do
+    chrxer <- withChannelReceiver chsink "proxy event"
+    data_writer <- withDataWriter src_data "proxy data"
+    taskBody $ \schedule ->
+      eventLoop schedule $ onChannel chrxer $ \val -> do
+        writeData schedule data_writer val
+  return snk_data
 

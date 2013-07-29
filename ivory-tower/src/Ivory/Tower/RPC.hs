25198d6e9c349c2f99caa4dfeaa5d26daec50f13
Pat Hickey <pat@galois.com>
2013-07-29 14:03:33 -0700

    rpc expanded a bit, not working yet

--- ivory-tower/src/Ivory/Tower/RPC.hs
+++ ivory-tower/src/Ivory/Tower/RPC.hs
@@ -9,24 +9,25 @@ module Ivory.Tower.RPC where
 import Control.Monad.Free
 
 import Ivory.Language
-import Ivory.Stdlib
 
 import Ivory.Tower.Types
-import Ivory.Tower.Monad
 import Ivory.Tower.Node
-import Ivory.Tower.Task
+import Ivory.Tower.Task ()
+import Ivory.Tower.Ivory
 
 
 -- AST
 data RPCF (t :: Area) (f :: Area) a
   = RPCLiftIvory (forall s . Ivory (ProcEffects s ()) ()) a
-  | RPCCall      (Ref Global t) (Ref Global f) a -- Input, Output
+  | RPCEmit      (forall s . ConstRef s t) a
+  | RPCReceive   (Ref Global f) a
   | RPCFreshName String (String -> a)
   | RPCModuleDef ModuleDef a
 
 instance Functor (RPCF t f) where
   fmap f (RPCLiftIvory i x) = RPCLiftIvory i (f x)
-  fmap f (RPCCall    i o x) = RPCCall    i o (f x)
+  fmap f (RPCEmit      i x) = RPCEmit      i (f x)
+  fmap f (RPCReceive   o x) = RPCReceive   o (f x)
   fmap f (RPCFreshName s k) = RPCFreshName s (f . k)
   fmap f (RPCModuleDef m x) = RPCModuleDef m (f x)
 
@@ -34,8 +35,8 @@ type RPC t f = Free (RPCF t f)
 
 -- Internal interface for writing ASTs:
 
-liftIvory :: (forall s . Ivory (ProcEffects s ()) ()) -> RPC t f ()
-liftIvory i = liftF $ RPCLiftIvory i ()
+rpcLiftIvory :: (forall s . Ivory (ProcEffects s ()) ()) -> RPC t f ()
+rpcLiftIvory i = liftF $ RPCLiftIvory i ()
 
 rpcFreshName :: String -> RPC t f String
 rpcFreshName s = liftF $ RPCFreshName s id
@@ -43,9 +44,11 @@ rpcFreshName s = liftF $ RPCFreshName s id
 rpcModuleDef :: ModuleDef -> RPC t f ()
 rpcModuleDef m = liftF $ RPCModuleDef m ()
 
-rpcMkCall :: Ref Global t -> Ref Global f -> RPC t f ()
-rpcMkCall i o = liftF $ RPCCall i o ()
+rpcEmit :: (forall s . ConstRef s t) -> RPC t f ()
+rpcEmit i = liftF $ RPCEmit i ()
 
+rpcReceive :: Ref Global f -> RPC t f ()
+rpcReceive o = liftF $ RPCReceive o ()
 
 -- Interpreter bindings to the rest of Tower.
 
@@ -60,51 +63,61 @@ rpc :: (SingI n, SingI m, IvoryArea t, IvoryArea f, IvoryZero t, IvoryZero f)
 rpc to fro name m = do
   emitter <- withChannelEmitter to ("rpc_to_" ++ name)
   receiver <- withChannelReceiver fro ("rpc_from_" ++ name)
-  let res = run m (emptyRPC 0) -- This guy needs:
-                                -- the value 'onChannel receiver'
-                                -- the value 'on'
+  let ctx = CgenCtx { ctx_emit = emit_ emitter }
+      res = cgen ctx m (emptyRPC 0)
   return (undefined res) -- punting here.
 
 -- Interpreter result. not really compelling
 
-data RPCRes =
+data RPCRes f =
   RPCRes
     { rpcres_moddef   :: ModuleDef
     , rpcres_frname   :: Int
-    , rpcres_states   :: forall s . [(Int, Ivory (ProcEffects s ()) ())] -- XXX this isn't really sufficient but i guess its better than nothing.
-    , rpcres_state    :: Int
+    , rpcres_receivers :: forall s s' . [(Int, ConstRef s' f -> Ivory (ProcEffects s ()) ())]
+    , rpcres_state     :: Int
     }
 
-emptyRPC :: Int -> RPCRes
+emptyRPC :: Int -> RPCRes f
 emptyRPC i =
   RPCRes
-    { rpcres_moddef   = return ()
-    , rpcres_frname   = i
-    , rpcres_states   = []
-    , rpcres_state    = 0
+    { rpcres_moddef    = return ()
+    , rpcres_frname    = i
+    , rpcres_receivers = []
+    , rpcres_state     = 0
+    }
+
+data CgenCtx (t :: Area) (f :: Area) =
+  CgenCtx
+    { ctx_emit :: forall s s' . ConstRef s' t -> Ivory (ProcEffects s ()) ()
     }
 
 -- Interpreter
 
-run :: RPC t f () -> RPCRes -> RPCRes
-run (Pure _)                  res = res -- XXX is this correct?? or should it be emptyResult?
+cgen :: CgenCtx t f -> RPC t f () -> RPCRes f -> RPCRes f
+cgen _   (Pure _)                  res = res -- XXX is this correct?? or should it be emptyResult?
 
-run (Free (RPCLiftIvory i c)) res = run c res'
+cgen ctx (Free (RPCLiftIvory i c)) res = cgen ctx c res'
   where
-  res' = res { rpcres_states = (rpcres_states res) ++ [s]}
-  s = (rpcres_state res, i)
+  res' = res { rpcres_receivers = (rpcres_receivers res) ++ [s]}
+  s = (rpcres_state res, const i)
 
-run (Free (RPCCall i o c)) res = run c res'
+cgen ctx (Free (RPCEmit      i c)) res = cgen ctx c res' -- XXX
   where
+  res' = res { rpcres_receivers = (rpcres_receivers res) ++ [s]}
+  s = (rpcres_state res, const $ ctx_emit ctx i)
+
+cgen ctx (Free (RPCReceive   o c)) res = cgen ctx c res'
+  where
+  -- Actually need to generate code here
   state' = (rpcres_state res) + 1
   res' = res { rpcres_state = state' }
 
-run (Free (RPCFreshName p k))   res = run (k name) res'
+cgen ctx (Free (RPCFreshName p k))   res = cgen ctx (k name) res'
   where
   res' = res { rpcres_frname = (rpcres_frname res) + 1 }
   name = (p ++ "_rpc" ++ (show (rpcres_frname res)))
 
-run (Free (RPCModuleDef m c)) res = run c res'
+cgen ctx (Free (RPCModuleDef m c)) res = cgen ctx c res'
   where
   res' = res { rpcres_moddef = (rpcres_moddef res) >> m }
 
@@ -114,8 +127,14 @@ simple :: RPC t f ()
 simple = do
   n1 <- rpcFreshName "name1"
   n2 <- rpcFreshName "name2"
-  rpcModuleDef $ defMemArea (area n1 Nothing :: MemArea (Stored Uint8))
-  rpcModuleDef $ defMemArea (area n2 Nothing :: MemArea (Stored Uint16))
+  let mem1 = area n1 Nothing :: MemArea (Stored Uint8)
+      ref1 = addrOf mem1
+      mem2 = area n2 Nothing :: MemArea (Stored Uint16)
+      ref2 = addrOf mem2
+  rpcModuleDef $ defMemArea mem1
+  rpcModuleDef $ defMemArea mem2
+  rpcLiftIvory $ store ref1 1
+  rpcLiftIvory $ store ref2 2
 
 -- Public interface should have one of these, and also expose liftIvory.
 

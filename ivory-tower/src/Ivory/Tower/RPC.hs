dda985a758980be0fe3a905355d7f50327802f03
Pat Hickey <pat@galois.com>
2013-07-31 14:27:27 -0700

    rpc monad is a writer

--- ivory-tower/src/Ivory/Tower/RPC.hs
+++ ivory-tower/src/Ivory/Tower/RPC.hs
@@ -1,3 +1,22 @@
+{-# LANGUAGE RankNTypes #-}
 
-module Ivory.Tower.RPC where
+module Ivory.Tower.RPC
+  ( RPCRunnable
+  , rpc
+  , runRPC
+  ) where
 
+import Ivory.Language
+
+import Ivory.Tower.Types
+
+import Ivory.Tower.RPC.AST
+import Ivory.Tower.RPC.Monad
+
+newtype RPCRunnable = RPCRunnable { unRPCRunnable :: forall eff . Ivory eff () }
+
+runRPC :: RPCRunnable -> Ivory eff ()
+runRPC = unRPCRunnable
+
+rpc :: ChannelSource n f -> ChannelSink m t -> RPC f t () -> Task RPCRunnable
+rpc = undefined
--- ivory-tower/src/Ivory/Tower/RPC/Monad.hs
+++ ivory-tower/src/Ivory/Tower/RPC/Monad.hs
@@ -3,6 +3,7 @@
 module Ivory.Tower.RPC.Monad where
 
 import MonadLib
+import Data.Monoid
 
 import Ivory.Language
 
@@ -11,7 +12,7 @@ import Ivory.Tower.RPC.AST
 
 newtype RPC f t a =
   RPC
-    { unRPC :: StateT (RPCSt t f) (Node TaskSt) a
+    { unRPC :: WriterT (RPCSt f t) (Node TaskSt) a
     } deriving (Functor, Monad)
 
 data RPCSt f t =
@@ -19,4 +20,22 @@ data RPCSt f t =
     { rpcst_moddef :: ModuleDef
     , rpcst_sm :: SM f t
     }
+
+instance Monoid (RPCSt f t) where
+  mempty      = RPCSt { rpcst_moddef = return ()
+                      , rpcst_sm     = mempty
+                      }
+  mappend a b = RPCSt { rpcst_moddef = rpcst_moddef a >> rpcst_moddef b
+                      , rpcst_sm     = rpcst_sm a <> rpcst_sm b
+                      }
+
+writeSM :: SM f t -> RPC f t ()
+writeSM s = RPC $ put RPCSt { rpcst_moddef = return (), rpcst_sm = s }
+
+writeModdef :: ModuleDef -> RPC f t ()
+writeModdef m = RPC $ put RPCSt { rpcst_moddef = m , rpcst_sm = mempty }
+
+instance BaseUtils (RPC f t) where
+  fresh = RPC $ lift fresh
+  getOS = RPC $ lift getOS
 

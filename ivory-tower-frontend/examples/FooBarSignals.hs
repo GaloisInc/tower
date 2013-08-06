3bfbe2f79bc3cc6dbe463b884ed73b78a4e31b35
Pat Hickey <pat@galois.com>
2013-08-06 13:37:51 -0700

    tower examples: all build with tower frontend

--- ivory-tower-frontend/examples/FooBarSignals.hs
+++ ivory-tower-frontend/examples/FooBarSignals.hs
@@ -1,14 +1,8 @@
 module Main where
 
-import qualified Ivory.Compile.C.CmdlineFrontend as C
-
-import Ivory.Tower.Graphviz
 import Ivory.Tower.Test.FooBarSignals
-import qualified Ivory.Tower.Compile.FreeRTOS as FreeRTOS
+import Ivory.Tower.Frontend
 
 main :: IO ()
-main = do
-  let (asm, objs) = FreeRTOS.compile fooBarTower
-  C.compileWith Nothing (Just [FreeRTOS.searchDir]) objs
-  graphvizToFile "fooBarSignalsTower.dot" asm
+main = compile defaultBuildConf fooBarTower
 
--- ivory-tower-frontend/examples/FooBarSimple.hs
+++ ivory-tower-frontend/examples/FooBarSimple.hs
@@ -1,14 +1,8 @@
 module Main where
 
-import qualified Ivory.Compile.C.CmdlineFrontend as C
-
-import Ivory.Tower.Graphviz
 import Ivory.Tower.Test.FooBarSimple
-import qualified Ivory.Tower.Compile.FreeRTOS as FreeRTOS
+import Ivory.Tower.Frontend
 
 main :: IO ()
-main = do
-  let (asm, objs) = FreeRTOS.compile fooBarTower
-  C.compileWith Nothing (Just [FreeRTOS.searchDir]) objs
-  graphvizToFile "fooBarTower.dot" asm
+main = compile defaultBuildConf fooBarTower
 

66931b60d291355753015365f16b296a5ab3211a
Pat Hickey <pat@galois.com>
2013-08-15 14:50:57 -0700

    ivory-tower-aadl: expand thread and port properties

--- ivory-tower-aadl/src/Ivory/Tower/Compile/AADL/Assembly.hs
+++ ivory-tower-aadl/src/Ivory/Tower/Compile/AADL/Assembly.hs
@@ -3,6 +3,8 @@ module Ivory.Tower.Compile.AADL.Assembly
   ( assemblyDoc
   ) where
 
+import System.FilePath
+
 import Ivory.Language
 import Ivory.Tower.Types
 
@@ -21,53 +23,69 @@ assemblyDoc name mods asm = runCompile mods virtMod $ do
 
 taskDef :: AssembledNode TaskSt -> CompileM ()
 taskDef asmtask = do
-  features <- featuresDef asmtask
-  writeThreadDefinition (t features)
+  features <- featuresDef asmtask (loopsource <.> "h")
+  writeThreadDefinition (ThreadDef n features props)
   where
-  t fs = ThreadDef name fs props
-  name = nodest_name (an_nodest asmtask)
-  props = []
+  n = nodest_name (an_nodest asmtask)
+  loopsource = "tower_task_loop_" ++ n
+  usersource = "tower_task_usercode_" ++ n
+  props = [ ThreadProperty "Source_Text" (dquotes (usersource <.> "c"))
+          ]
 
 signalDef :: AssembledNode SignalSt -> CompileM ()
 signalDef asmsig = do
-  features <- featuresDef asmsig
-  writeThreadDefinition (t features)
+  features <- featuresDef asmsig (commsource <.> "h")
+  writeThreadDefinition (ThreadDef n features props)
   where
-  t fs = ThreadDef name fs props
-  name = nodest_name (an_nodest asmsig)
-  props = []
+  n = nodest_name (an_nodest asmsig)
+  commsource = "tower_signal_comm_" ++ n 
+  usersource = "tower_signal_usercode_" ++ n
+  props = [ ThreadProperty "Source_Text" (dquotes (usersource <.> "c"))
+          ]
 
-featuresDef :: AssembledNode a -> CompileM [ThreadFeature]
-featuresDef an = do
+featuresDef :: AssembledNode a -> FilePath -> CompileM [ThreadFeature]
+featuresDef an headername = do
   ems <- mapM emitterDef    (nodees_emitters    edges)
   rxs <- mapM receiverDef   (nodees_receivers   edges)
   wrs <- mapM dataWriterDef (nodees_datawriters edges)
   res <- mapM dataReaderDef (nodees_datareaders edges)
-  return (ems ++ rxs ++ wrs ++ res)
+  return $ concat [ems, rxs, wrs, res]
 
   where
   edges = nodest_edges (an_nodest an)
-  props = [("thread_feature_propertes","punted_on")] -- XXX
+  props sourcetext =
+    [ ("Compute_Entrypoint_Source_Header", dquotes headername)
+    , ("Compute_Entrypoint_Source_Text", dquotes sourcetext)
+    ]
+  channelprops sourcetext len = ("Queue_Size", show len) : props sourcetext
 
   emitterDef :: Labeled ChannelId -> CompileM ThreadFeature
-  emitterDef (Labeled e n) = do
-    t <- channelTypename e
-    return $ ThreadFeaturePort (identifier n) PortKindEvent Out t props
+  emitterDef lc = do
+    chtype <- channelTypename ch
+    return $ ThreadFeaturePort portname PortKindEvent Out chtype ps
+    where
+    ch = unLabeled lc
+    portname = identifier (lbl_user lc)
+    ps = channelprops (lbl_code lc) (chan_size ch)
 
   receiverDef :: Labeled ChannelId -> CompileM ThreadFeature
-  receiverDef (Labeled e n) = do
-    t <- channelTypename e
-    return $ ThreadFeaturePort (identifier n) PortKindEvent In t props
+  receiverDef lc = do
+    chtype <- channelTypename ch
+    return $ ThreadFeaturePort portname PortKindEvent In chtype ps
+    where
+    ch = unLabeled lc
+    portname = identifier (lbl_user lc)
+    ps = channelprops (lbl_code lc) (chan_size ch)
 
   dataWriterDef :: Labeled DataportId -> CompileM ThreadFeature
-  dataWriterDef (Labeled e n) = do
+  dataWriterDef (Labeled e n c) = do
     t <- dataportTypename e
-    return $ ThreadFeaturePort (identifier n) PortKindData Out t props
+    return $ ThreadFeaturePort (identifier n) PortKindData Out t (props c)
 
   dataReaderDef :: Labeled DataportId -> CompileM ThreadFeature
-  dataReaderDef (Labeled e n) = do
+  dataReaderDef (Labeled e n c) = do
     t <- dataportTypename e
-    return $ ThreadFeaturePort (identifier n) PortKindData In t props
+    return $ ThreadFeaturePort (identifier n) PortKindData In t (props c)
 
 channelTypename :: ChannelId -> CompileM TypeName
 channelTypename chid = do
@@ -84,4 +102,7 @@ implNonBaseTypes :: TypeName -> TypeName
 implNonBaseTypes t = case t of
   QualTypeName "Base_Types" _ -> t
   _ -> DotTypeName t "impl"
+
+dquotes :: String -> String
+dquotes s = "\"" ++ s ++ "\""
 

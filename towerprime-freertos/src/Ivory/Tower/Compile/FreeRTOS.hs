9879f1feabd874000cf76d20ceeb8065b632708f
Pat Hickey <pat@galois.com>
2014-03-03 15:21:07 -0800

    towerprime-freertos: stub out new OS interface

--- towerprime-freertos/src/Ivory/Tower/Compile/FreeRTOS.hs
+++ towerprime-freertos/src/Ivory/Tower/Compile/FreeRTOS.hs
@@ -1,25 +1,70 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
 
 module Ivory.Tower.Compile.FreeRTOS
   ( os
-  , compile
   , searchDir
   ) where
 
 import           Ivory.Language
 import           Ivory.Tower
 import qualified Ivory.Tower.AST as AST
-import           Ivory.Tower.Types.OS
-import           Ivory.Tower.Types.Assembly
+import qualified Ivory.Tower.Types.OS as OS
+import           Ivory.Tower.Types.TaskCode
+import           Ivory.Tower.Types.SystemCode
 
-import Ivory.Tower.Compile.FreeRTOS.SearchDir
+import Ivory.Tower.Compile.FreeRTOS.SearchDir (searchDir)
 
-os :: OS
-os = OS
-  { gen_channel = \sys chan proxy -> (proc "garbage" $ body $ return (), return ())
-  , get_emitter = \sys chan ref -> return ()
-  , get_receiver = \sys task chan ref -> return false
+os :: OS.OS
+os = OS.OS
+  { OS.gen_channel     = gen_channel
+  , OS.get_emitter     = get_emitter
+  , OS.get_receiver    = get_receiver
+  , OS.codegen_task    = codegen_task
+  , OS.codegen_sysinit = codegen_sysinit
   }
 
-compile :: Tower () -> (Assembly, [Module])
-compile _ = error "freertos.compile undefined"
+gen_channel :: (IvoryArea area, IvoryZero area)
+            => AST.System
+            -> AST.Chan
+            -> Proxy area
+            -> (Def('[]:->()), ModuleDef)
+gen_channel sys chan proxy = garbage
+  where
+  garbage = (proc "garbage" $ body $ return (), return ())
+
+get_emitter :: (IvoryArea area, IvoryZero area)
+            => AST.System
+            -> AST.Chan
+            -> ConstRef s area
+            -> Ivory eff ()
+get_emitter sys chan = garbage
+  where
+  garbage ref = return ()
+
+get_receiver :: (IvoryArea area, IvoryZero area)
+             => AST.System
+             -> AST.Task
+             -> AST.Chan
+             -> Ref s area
+             -> Ivory eff IBool
+get_receiver sys task chan = garbage
+  where
+  garbage ref = return false
+
+codegen_task :: AST.System
+             -> AST.Task
+             -> TaskCode
+             -> ([Module],ModuleDef)
+codegen_task sys task taskcode = garbage
+  where
+  garbage = ([],return ())
+
+codegen_sysinit :: AST.System
+                -> SystemCode
+                -> ModuleDef
+                -> [Module]
+codegen_sysinit sysast syscode taskmoddefs = garbage
+  where
+  garbage = []
 

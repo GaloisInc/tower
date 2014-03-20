c631d0de95e78651a7ec65e0d6a0a4def8c25848
Pat Hickey <pat@galois.com>
2014-03-19 17:21:59 -0700

    towerprime-examples: add trivial signal example app

---
+++ towerprime-examples/examples/SimpleSignal.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+import Ivory.Language
+import Ivory.Stdlib
+import Ivory.Tower
+import Ivory.Tower.Frontend
+
+data ExamplePlatform = ExamplePlatform
+instance Signalable ExamplePlatform where
+  data SignalType ExamplePlatform = ExampleSignal1
+                                  | ExampleSignal2
+                                  deriving (Eq, Show, Read)
+  signals = [ExampleSignal1, ExampleSignal2]
+  signalName = show
+  signalFromName = read
+  signalSources = const []
+  signalHeaders = const []
+  signalSearchDir = const (return [])
+
+
+task_simple_per :: Task p ()
+task_simple_per = do
+  ctr <- taskLocal "counter"
+  lasttime <- taskLocal "lasttime"
+  p <- timerEvent (Milliseconds 100)
+
+  handle p "periodic" $ \timeRef -> do
+    deref timeRef >>= store lasttime
+    deref ctr >>= \(c :: Sint32) -> store ctr (c + 1)
+
+task_simple_sig :: Task ExamplePlatform ()
+task_simple_sig = do
+  ctr <- taskLocal "counter"
+  p <- withSignalEvent ExampleSignal1 "signal1"
+  handle p "signalhandler" $ \_ -> do
+    deref ctr >>= \(c :: Sint32) -> store ctr (c + 1)
+
+tower_simple_per_tasks :: Tower ExamplePlatform ()
+tower_simple_per_tasks = do
+  task "per_trivial" task_simple_per
+  task "sig_trivial" task_simple_sig
+
+
+main :: IO ()
+main = compile defaultBuildConf tower_simple_per_tasks
+

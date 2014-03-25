71a7702a65684e07ce5588ed1eea5526e1e1c436
Pat Hickey <pat@galois.com>
2014-03-25 11:00:51 -0700

    deprecate old tower.
    keep aadl and echronos backends available, but disabled from the build.

--- ivory-tower-freertos/ivory-freertos-wrapper/freertos_atomic_wrapper.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __IVORY_FREERTOS_ATOMIC_WRAPPER_H__
-#define __IVORY_FREERTOS_ATOMIC_WRAPPER_H__
-
-void ivory_freertos_begin_atomic(void);
-void ivory_freertos_end_atomic(void);
-
-#endif // __IVORY_FREERTOS_ATOMIC_WRAPPER_H__

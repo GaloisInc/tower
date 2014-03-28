ecef818f801daa5706d38c7aedf3060d82e44ed5
Pat Hickey <pat@galois.com>
2014-03-27 18:55:47 -0700

    tower-statemachine: initial commit. derived from tower 0.7. not working yet
    need to implement taskChannel :: Task p (ChannelSource a, ChannelSink a),
    which isnt easy with the current hierarchy of Task and Tower both sitting on
    Base. I'd need to rebase Task on Tower, or keep around some extra side effects
    just for the sake of holding onto generated channels.
    
    rebasing task onto tower means merging the two into a single haskell module
    because currently tower depends on task.
    
    sticky wicket, thats for sure

---
+++ tower-statemachine/src/Ivory/Tower/StateMachine.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecursiveDo #-}
+
+module Ivory.Tower.StateMachine
+  ( on
+  , period
+  , timeout
+  , entry
+
+  , Runnable
+  , active
+  , begin
+  , stateMachine
+
+  , MachineM
+  , Stmt
+
+  , state
+  , stateNamed
+  , branch
+  , goto
+  , haltWhen
+  , halt
+  , liftIvory
+  , liftIvory_
+  ) where
+
+import Ivory.Language
+import Ivory.Tower
+import Ivory.Tower.StateMachine.Monad
+import Ivory.Tower.StateMachine.Compile
+
+on :: forall area
+         . (IvoryArea area, IvoryZero area)
+        => Event area
+        -> (forall s s' . ConstRef s' area -> StmtM s ())
+        -> StateM ()
+on e stmtM = writeHandler $ EventHandler e (ScopedStatements stmts)
+  where
+  stmts :: ConstRef s' area -> [Stmt s]
+  stmts ref = runStmtM (stmtM ref)
+
+entry :: (forall s . StmtM s ()) -> StateM ()
+entry stmtM = writeHandler $ EntryHandler (ScopedStatements (const (runStmtM stmtM)))
+
+timeout :: Int -> (forall s . StmtM s ()) -> StateM ()
+timeout t stmtM = writeHandler $ TimeoutHandler t (ScopedStatements (const (runStmtM stmtM)))
+
+period :: Int -> (forall s . StmtM s ()) -> StateM ()
+period t stmtM = writeHandler $ PeriodHandler t (ScopedStatements (const (runStmtM stmtM)))
+
+state :: StateM () -> Machine
+state sh = stateAux sh Nothing
+
+stateNamed :: String -> StateM () -> Machine
+stateNamed n sh = stateAux sh (Just n)
+
+stateAux :: StateM () -> Maybe String-> Machine
+stateAux sh name = do
+  l <- label
+  writeState $ runStateM sh l name
+  return l
+
+branch :: (CFlowable m) => IBool -> StateLabel -> m ()
+branch b lbl = cflow $ CFlowBranch b lbl
+
+goto :: (CFlowable m) => StateLabel -> m ()
+goto lbl = branch true lbl
+
+haltWhen :: (CFlowable m) => IBool -> m ()
+haltWhen p = cflow $ CFlowHalt p
+
+halt :: (CFlowable m) => m ()
+halt = haltWhen true
+
+liftIvory_ :: Ivory (AllocEffects s) () -> StmtM s ()
+liftIvory_ i = writeStmt $ Stmt (i >> return (return ()))
+
+liftIvory :: Ivory (AllocEffects s) CFlow-> StmtM s ()
+liftIvory i = writeStmt $ Stmt i
+
---
+++ tower-statemachine/src/Ivory/Tower/StateMachine/Compile.hs
@@ -0,0 +1,152 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
+
+module Ivory.Tower.StateMachine.Compile where
+
+import Control.Monad (forM_)
+
+import Ivory.Tower.StateMachine.Monad
+
+import Ivory.Language
+import Ivory.Tower
+import Ivory.Stdlib
+
+nodeChannel :: Task p (ChannelSource area, ChannelSink area)
+nodeChannel = undefined "nodechannel nope"
+
+data Runnable =
+  Runnable
+    { runnable_begin  :: Def ('[]:->())
+    , runnable_active :: Def ('[]:->IBool)
+    }
+
+begin :: Runnable -> Ivory eff ()
+begin r = call_ (runnable_begin r)
+
+active :: Runnable -> Ivory eff IBool
+active r = call (runnable_active r)
+
+stateMachine :: String -> MachineM StateLabel -> Task p Runnable
+stateMachine name machine = do
+  uniq <- freshname ("machine_" ++ name)
+  tick <- timerEvent (Milliseconds 1)
+  newstate <- nodeChannel -- XXX XXX DOES NOT EXIST
+  newstate_emitter <- withChannelEmitter (src newstate) "newstateEmitter"
+  newstate_receiver <- withChannelEvent (snk newstate) "newstateEvent"
+  aux uniq tick newstate_emitter newstate_receiver
+  where
+  (istate, states) = runMachineM machine
+  aux :: Unique
+      -> Event (Stored ITime)
+      -> ChannelEmitter (Stored Uint32)
+      -> Event (Stored Uint32)
+      -> Task p Runnable
+  aux uniq tick newstate_emitter newstate_evt = do
+    mapM_ mkState states
+    taskModuleDef moddef
+    return runner
+    where
+    named n = (showUnique uniq) ++ "_" ++ n
+    runner = Runnable
+      { runnable_begin = begin_proc
+      , runnable_active = active_proc
+      }
+    moddef = do
+      incl begin_proc
+      incl active_proc
+      defMemArea active_area
+      defMemArea state_area
+
+    active_area = area (named "state_active")      (Just (ival false))
+    state_area  = area (named "state")             (Just (ival 0))
+
+    (activeState     :: Ref Global (Stored IBool))  = addrOf active_area
+    (state           :: Ref Global (Stored Uint32)) = addrOf state_area
+
+    begin_proc = proc (named "begin") $ body $ do
+      a <- deref activeState
+      unless a $ noReturn $ do
+        store activeState true
+        nextstate istate
+
+    active_proc = proc (named "active") $ body $
+      deref activeState >>= ret
+
+    nextstate :: StateLabel -> Ivory (AllocEffects s) ()
+    nextstate (StateLabel ns) = do
+      -- The first use of nextstate in an event cycle will be the one to be
+      -- accepted, since newstate_emitter comes from a channel of size 1.
+      emitV_ newstate_emitter (fromIntegral ns)
+
+    handleState :: StateLabel -> Ivory (AllocEffects s) () -> Ivory (ProcEffects s ()) ()
+    handleState (StateLabel s) k = do
+      current <- deref state
+      when (current ==? (fromIntegral s)) (noReturn k)
+
+    mkState :: State -> Task p ()
+    mkState (State s maybename handlers) = do
+      ehs <- mapM mkHandler handlers
+      handle newstate_evt nsname $ \newstref -> noReturn $ do
+        newst <- deref newstref
+        when (newst ==? (fromIntegral (unStateLabel s))) $ do
+          store state newst
+          now <- getTime
+          currenttime <- local (ival now)
+          forM_ ehs $ \(ScopedStatements ss) ->
+            mapM_ mkStmt (ss (constRef currenttime))
+      where
+      nsname = case maybename of
+        Just n -> "newstate_" ++ n
+        Nothing -> "newstate"
+      onEvt :: (IvoryArea area, IvoryZero area)
+            => Event area
+            -> (forall s s' . ConstRef s area -> Ivory (ProcEffects s' ()) ())
+            -> Task p ()
+      onEvt e k = case maybename of
+        Just n  -> handle e n k
+        Nothing -> handle e "statemachineevent" k
+      mkHandler (EntryHandler stmts) = return stmts -- XXX correct?
+      mkHandler (TimeoutHandler i stmts) = do
+        due <- taskLocal (named ("timeoutDue" ++ show i))
+        onEvt tick $ \currenttime -> handleState s $ do
+          now <- deref currenttime
+          d <- deref due
+          when ((d >? 0) .&& (d <=? now)) $ case stmts of
+            ScopedStatements ss -> do
+              mapM_ mkStmt (ss currenttime)
+              store due 0
+        return $ scopedIvory $ \starttimeRef -> do
+          t <- deref starttimeRef
+          store due (t + (fromIntegral i))
+
+      mkHandler (PeriodHandler i stmts) = do
+        due <- taskLocal (named ("periodDue" ++ show i))
+        onEvt tick $ \currenttime -> case stmts of
+          ScopedStatements ss -> handleState s $ do
+            now <- deref currenttime
+            d   <- deref due
+            when (d <=? now) $ do
+              mapM_ mkStmt (ss currenttime)
+              store due (d + (fromIntegral i))
+        return $ scopedIvory $ \starttimeRef -> do
+          t <- deref starttimeRef
+          store due (t + (fromIntegral i))
+
+      mkHandler (EventHandler evt stmts) = do
+        onEvt evt $ case stmts of
+          ScopedStatements ss -> \v -> handleState s $ do
+            mapM_ mkStmt (ss v)
+        return $ scopedIvory $ const (return ())
+
+    mkStmt :: Stmt s -> Ivory (AllocEffects s) ()
+    mkStmt (Stmt s) = do
+      cflowM <- s
+      foldr mkCFlow (return ()) (runCFlowM cflowM)
+
+    mkCFlow :: CFlowAST -> Ivory (AllocEffects s) () -> Ivory (AllocEffects s) ()
+    mkCFlow (CFlowBranch b lbl) acc = ifte_ b (nextstate lbl) acc
+    mkCFlow (CFlowHalt   b)     acc = ifte_ b (store activeState false) acc
+
+

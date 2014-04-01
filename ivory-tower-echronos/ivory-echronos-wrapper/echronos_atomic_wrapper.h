65c3645ece01f45137497bf81235e8801314d66e
Pat Hickey <pat@galois.com>
2014-04-01 15:52:09 -0700

    remove echronos backend (deprecated)

--- ivory-tower-echronos/ivory-echronos-wrapper/echronos_atomic_wrapper.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __IVORY_ECHRONOS_ATOMIC_WRAPPER_H__
-#define __IVORY_ECHRONOS_ATOMIC_WRAPPER_H__
-
-void ivory_echronos_begin_atomic(void);
-void ivory_echronos_end_atomic(void);
-
-#endif // __IVORY_ECHRONOS_ATOMIC_WRAPPER_H__

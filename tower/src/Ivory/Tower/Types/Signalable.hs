4516a8749fd55ae7f08bec85f9368c80ca7355eb
Pat Hickey <pat@galois.com>
2014-06-03 11:25:42 -0700

    tower: remove unused methods from signalable class

--- tower/src/Ivory/Tower/Types/Signalable.hs
+++ tower/src/Ivory/Tower/Types/Signalable.hs
@@ -7,19 +7,11 @@ module Ivory.Tower.Types.Signalable
 
 class Signalable p where
   data SignalType p
-  signals         :: [SignalType p]
-  -- each value of SignalType p must be transferable to/from strings without
-  -- losing information.
-  signalName      :: SignalType p -> String
-  -- signalFromName is partial, user is burdened with only giving valid names
-  -- (i.e. those produced by signalName)
-  signalFromName  :: String -> SignalType p
+  signalName :: SignalType p -> String
 
 -- Utility class
 data NoSignals = NoSignals
 instance Signalable NoSignals where
   data SignalType NoSignals = NoSignal deriving (Show)
-  signals = [NoSignal]
   signalName = show
-  signalFromName = const NoSignal
 

2242d5504f7e8bb944882752d311dc44b2b6860c
Pat Hickey <pat@galois.com>
2014-07-21 18:32:59 -0700

    grtos: task_create exported by kernel, create a task in trivial test

--- grtos/src/Ivory/GRTOS.hs
+++ grtos/src/Ivory/GRTOS.hs
@@ -6,20 +6,52 @@
 
 module Ivory.GRTOS where
 
+import Data.String (fromString)
 import Ivory.Language
-import Ivory.Compile.C.CmdlineFrontend (compile)
+import Ivory.Compile.C.CmdlineFrontend (compileWith)
 
 import Ivory.GRTOS.PriorityGroup
 import Ivory.GRTOS.AST
+import Ivory.GRTOS.Kernel
+import Ivory.GRTOS.Kernel.TaskControlBlock
+import Ivory.GRTOS.SearchDir
 
 test :: IO ()
-test = compile [m]
+test = compileWith Nothing (Just [searchDir])  [m, launch, kernel, taskControlBlockTypeModule]
   where
-  code pc = package "pkg" $ pg_moduledef pc
+  launch = package "launch" $ do
+    depend taskControlBlockTypeModule
+    depend m
+    defMemArea task_tcb_area
+    defMemArea task_stack_area
+    incl launch_proc
+
+  task_tcb_area :: MemArea (Struct "task_control_block")
+  task_tcb_area = area "sometask_tcb" Nothing
+  task_tcb = addrOf task_tcb_area
+
+  task_stack_area :: MemArea (Array 256 (Stored Uint32))
+  task_stack_area = area "sometask_stack" Nothing
+  task_stack = addrOf task_stack_area
+
+  taskname :: IString
+  taskname = fromString "sometask"
+
+  launch_proc  :: Def('[]:->())
+  launch_proc = proc "launch_proc" $ body $ do
+    call_ kernel_task_create
+              task_tcb                -- globally allocated control block
+              (procPtr loop)          -- non-terminating task body
+              (toCArray task_stack)   -- uint32_t *stack_start
+              (256 * 4)               -- sizeof(task_stack_area)
+              taskname                -- name
+              1                       -- priority
+
+  code pc = (package "pkg" $ pg_moduledef pc, pg_loop pc)
 
   -- XXX is there anything in the ghc 7.8's typelits that can do this
   -- dispatch for us, e.g. a magic dictionary
-  m = case ((length es) `div` 32) + 1 of
+  (m, loop) = case ((length es) `div` 32) + 1 of
         1 -> code (priorityGroup (Priority 1 es) :: PriorityGroup 1)
         2 -> code (priorityGroup (Priority 1 es) :: PriorityGroup 2)
         3 -> code (priorityGroup (Priority 1 es) :: PriorityGroup 3)
--- grtos/src/Ivory/GRTOS/Kernel.hs
+++ grtos/src/Ivory/GRTOS/Kernel.hs
@@ -5,6 +5,7 @@
 module Ivory.GRTOS.Kernel where
 
 import Ivory.Language
+import Ivory.GRTOS.Kernel.TaskControlBlock
 
 kernel_wait :: Def('[]:->())
 kernel_wait = importProc "kernel_wait" kernel_header
@@ -20,6 +21,15 @@ kernel_end_atomic = importProc "kernelENABLE_INTERRUPTS" kernel_header
 
 kernel_scheduler_start :: Def('[]:->())
 kernel_scheduler_start = importProc "scheduler_start" kernel_header
+
+kernel_task_create :: Def('[ Ref s0 (Struct "task_control_block")
+                           , ProcPtr ('[]:->())
+                           , Ref s1 (CArray (Stored Uint32))
+                           , Uint32
+                           , IString
+                           , Uint8
+                           ] :->()) -- XXX incomplete
+kernel_task_create = importProc "task_create" kernel_header
 
 kernel_header :: String
 kernel_header = "grtos_kernel.h"

9702b631e36b4c00982fa2d111bbf654501d4df0
Pat Hickey <pat@galois.com>
2014-10-13 11:43:57 -0700

    test code uses monitor state

--- src/Ivory/Tower/Test.hs
+++ src/Ivory/Tower/Test.hs
@@ -11,10 +11,12 @@ test1 = do
   (c1in, c1out) <- channel
   per <- period (Microseconds 1000)
   monitor "m1" $ do
+    _ <- state "some_m1_state"
     handler per "tick" $ do
       emitter c1in 1
       callback "some_ivory_in_m1_tick"
   monitor "m2" $ do
+    _ <- state "some_m2_state"
     handler c1out "chan1msg" $ do
       callback "some_ivory_in_m2_onmsg"
 
@@ -24,20 +26,24 @@ test2 = do
   (c2in, c2out) <- channel
   per <- period (Microseconds 1000)
   monitor "m1" $ do
+    _ <- state "some_m1_state"
     handler per "tick" $ do
       emitter c1in 1
       callback "some_ivory_in_m1_tick"
   monitor "m2" $ do
+    _ <- state "some_m2_state"
     handler c1out "chan1msg" $ do
       callback "some_ivory_in_m2_onmsg"
       emitter c2in 1
   monitor "m3" $ do
+    _ <- state "some_m3_state"
     handler c1out "chan1msg" $ do
       callback "some_ivory_in_m3_onchan1"
       emitter c2in 1
     handler c2out "chan2msg" $ do
       callback "some_ivory_in_m3_onchan2"
   monitor "m4" $ do
+    _ <- state "some_m4_state"
     handler c2out "chan2msg" $ do
       callback "some_ivory_in_m4_onmsg"
 

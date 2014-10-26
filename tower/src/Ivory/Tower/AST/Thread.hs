6b9e2265850f3b63fe6681c07bf7c2a9395089d7
Pat Hickey <pat@galois.com>
2014-10-25 17:29:44 -0700

    tower: thread Ord based on deadline with appropriate tiebreakers

--- tower/src/Ivory/Tower/AST/Thread.hs
+++ tower/src/Ivory/Tower/AST/Thread.hs
@@ -1,5 +1,10 @@
 
-module Ivory.Tower.AST.Thread where
+module Ivory.Tower.AST.Thread
+  ( Thread(..)
+  , threadName
+  , threadChan
+  , threadDeadline
+  ) where
 
 import Ivory.Tower.Types.Time
 import Ivory.Tower.AST.Period
@@ -8,7 +13,7 @@ import Ivory.Tower.AST.Chan
 
 data Thread = SignalThread Signal
             | PeriodThread Period
-            deriving (Eq, Show, Ord)
+            deriving (Eq, Show)
 
 threadName :: Thread -> String
 threadName (SignalThread s) = "thread_signal_" ++ signal_name s
@@ -27,3 +32,17 @@ threadChan (SignalThread s) = ChanSignal s
 threadDeadline :: Thread -> Microseconds
 threadDeadline (PeriodThread p) = period_dt p
 threadDeadline (SignalThread s) = signal_deadline s
+
+instance Ord Thread where
+  compare a b
+    | threadDeadline a == threadDeadline b = tiebreak a b
+    | otherwise = compare (threadDeadline a) (threadDeadline b)
+
+tiebreak :: Thread -> Thread -> Ordering
+-- Break tie between signal and period - period is greater
+tiebreak (SignalThread _)  (PeriodThread _) = LT
+tiebreak (PeriodThread _) (SignalThread _) = GT
+-- Break tie between signals - based on signal name
+tiebreak (SignalThread (Signal a _)) (SignalThread (Signal b _)) = compare a b
+-- Periods are a set, should not need to break ties.
+tiebreak (PeriodThread _) (PeriodThread _) = EQ

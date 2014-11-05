3b63004a237151382020a1e0b8eeb9dd6d297258
Pat Hickey <pat@galois.com>
2014-11-05 15:33:53 -0800

    tower-examples: produces test of ivory-freertos-bindings that builds objects
    - Might be nice to put a simple app that exercises all the freertos kernel
      features we need in here, if we ever need to make any sort of kernel or
      platform changes...

--- tower-examples/freertos-bindings-test/Main.hs
+++ tower-examples/freertos-bindings-test/Main.hs
@@ -1,14 +1,17 @@
 
 module Main where
 
-import Ivory.OS.FreeRTOS.Artifacts
-import Ivory.Compile.C.CmdlineFrontend
+import qualified Data.List as L
 
+import Ivory.Artifact
+import Ivory.OS.FreeRTOS
+import Ivory.Compile.C.CmdlineFrontend
 
 main :: IO ()
 main = compile [] as
   where
   as = kernel config ++ wrappers
+     ++ [ artifactString "Makefile" makefile ]
   config = Config
     { cpu_clock_hz = 168 * 1000 * 1000
     , tick_rate_hz = 1000
@@ -16,4 +19,33 @@ main = compile [] as
     , minimal_stack_size = 512
     , total_heap_size = 64 * 1024 * 1024
     }
+
+makefile :: String
+makefile = unlines
+  [ "CC := arm-none-eabi-gcc"
+  , "CFLAGS := \\"
+  , "  -g3 -Wall -Werror -O2 \\"
+  , "  -std=gnu99 \\"
+  , "  -Wno-parenthesis -Wno-unused-variable \\"
+  , "  -mlittle-endian \\"
+  , "  -mthumb -mcpu=cortex-m4 \\"
+  , "  -mfloat-abi=hard -mfpu=fpv4-sp-d16"
+  , ""
+  , "OBJDIR := obj"
+  , "OBJS := $(addprefix $(OBJDIR)/," ++ (L.intercalate " " objects) ++ ")"
+  , ""
+  , "default: $(OBJDIR) $(OBJS)"
+  , ""
+  , "$(OBJDIR)/%.o : %.c"
+  , "\t$(CC) $(CFLAGS) -c -o $@ $<"
+  , ""
+  , "$(OBJDIR):"
+  , "\tmkdir -p $(OBJDIR)"
+  , ""
+  , "clean:"
+  , "\t-rm -rf obj"
+  , ""
+  , "veryclean: clean"
+  , "\t-rm *.c *.h"
+  ]
 

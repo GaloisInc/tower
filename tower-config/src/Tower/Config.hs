8bd8be29843876c07f36028e369f561efcb6ebbd
Pat Hickey <pat@galois.com>
2014-11-13 16:21:53 -0800

    tower-config: getConfig does opt parsing and gets document

--- tower-config/src/Tower/Config.hs
+++ tower-config/src/Tower/Config.hs
@@ -1,7 +1,21 @@
 
 module Tower.Config
   ( module Tower.Config.Types
+  , getConfig
   ) where
 
+import Ivory.Tower.Compile.Options
 import Tower.Config.Types
+import Tower.Config.Options
+import Tower.Config.Document
+
+getConfig :: TOpts -> IO (Value, TOpts)
+getConfig topts = do
+  (cfgopts, t') <- getCfgOpts topts
+  d <- getDocument (cfgopts_configfile cfgopts)
+                   (cfgopts_configpath cfgopts)
+  case d of
+    Right v -> return (v, t')
+    Left e -> topts_error t' ("Error in tower getConfig: " ++ e)
+
 

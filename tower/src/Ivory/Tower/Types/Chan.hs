e94d7db1b5975ec6b8fbcb0f0d3f271a4336267f
Pat Hickey <pat@galois.com>
2014-11-26 17:49:58 -0800

    tower: Chan types derive Eq

--- tower/src/Ivory/Tower/Types/Chan.hs
+++ tower/src/Ivory/Tower/Types/Chan.hs
@@ -8,7 +8,10 @@ import qualified Ivory.Tower.AST as AST
 import Ivory.Language
 
 data Chan (a :: Area *) = Chan AST.Chan
+  deriving (Eq)
 
 newtype ChanInput (a :: Area *) = ChanInput (Chan a)
-newtype ChanOutput (a :: Area *) = ChanOutput (Chan a)
+  deriving (Eq)
 
+newtype ChanOutput (a :: Area *) = ChanOutput (Chan a)
+  deriving (Eq)

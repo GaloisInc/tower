6f59009b2e1cae40b7469c2f4f5ab76d795b3fd9
Pat Hickey <pat@galois.com>
2014-12-01 17:49:01 -0800

    tower-config: fix bug overriding config from cmdline options

--- tower-config/src/Tower/Config.hs
+++ tower-config/src/Tower/Config.hs
@@ -6,7 +6,7 @@ module Tower.Config
   ) where
 
 import Control.Monad (when)
-import Ivory.Tower.Compile.Options
+import Ivory.Tower.Compile.Options hiding (parseOpts)
 import Tower.Config.Parser
 import Tower.Config.Options
 import Tower.Config.Document
@@ -18,19 +18,28 @@ import Tower.Config.TOML
 getConfig' :: TOpts -> ConfigParser a -> IO (a, TOpts)
 getConfig' topts p = do
   (cfgopts, t') <- getCfgOpts topts
-  let dbg s = when (cfgopts_debug cfgopts) (putStrLn s)
+  let dbg s = when (cfgopts_debug cfgopts) (putStrLn ("DBG: " ++ s))
   d <- getDocument (cfgopts_configfile cfgopts)
                    (cfgopts_configpath cfgopts)
   case d of
     Left e -> topts_error t' ("Error in tower getConfig: " ++ e)
     Right toml -> do
-      dbg ("base document: " ++ (show toml))
-      let extendedV :: Value
-          extendedV = Left (extendConfig toml (topts_args topts))
-      dbg ("document with cmdargs: " ++ (show extendedV))
-      case runConfigParser p extendedV of
+      dbg ("base toml document:" ++ (debugtoml (Left toml)))
+      let extra_opts :: [String]
+          extra_opts = topts_args t'
+          ext_toml :: Value
+          ext_toml = Left (extendConfig toml extra_opts)
+      dbg ("cfg extra args: " ++ (show extra_opts))
+      dbg ("parsed extra args to: " ++ (show (parseOpts extra_opts)))
+      dbg ("extended toml document:" ++ (debugtoml ext_toml))
+      case runConfigParser p ext_toml of
         Right c -> return (c, t')
         Left  e -> topts_error t' ("Error parsing config file: " ++ e)
+
+  where
+  debugtoml :: Value -> String
+  debugtoml v = sep ++ ppValue v ++ sep
+  sep = "\n==================\n"
 
 getConfig :: TOpts -> ConfigParser a -> IO a
 getConfig topts p = fmap fst (getConfig' topts p)

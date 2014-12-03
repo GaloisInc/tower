1f3300368c736bdc9755c5b7bde0f2556345f68b
Pat Hickey <pat@galois.com>
2014-12-03 11:49:55 -0800

    tower-config: support optional includes

--- tower-config/src/Tower/Config/Preprocess.hs
+++ tower-config/src/Tower/Config/Preprocess.hs
@@ -8,15 +8,16 @@ import qualified Data.ByteString.Char8 as B
 import System.FilePath
 import System.Directory
 
-data Line = Literal B.ByteString | Include FilePath
+data Line = Literal B.ByteString | Include Importance FilePath
+data Importance = Mandatory | Optional
 
-includeDirective :: B.ByteString -> Maybe FilePath
-includeDirective l =
+directive :: String -> B.ByteString -> Maybe FilePath
+directive tag l =
   case (h == B.empty, t' == B.empty) of
     (True, True) -> Just (B.unpack h')
     _ -> Nothing
   where
-  (h , t ) = beforeafter (B.pack "#include \"") l
+  (h , t ) = beforeafter (B.pack ("#" ++ tag ++ " \"")) l
   (h', t') = beforeafter (B.pack "\"") t
   beforeafter s m = let (b, a) = B.breakSubstring s m
                         a' = B.drop (B.length s) a
@@ -27,9 +28,12 @@ getLines f = do
   b <- B.readFile f
   let ls = B.lines b
   return (map mkln ls)
-  where mkln b = case includeDirective b of
-                    Just s -> Include s
-                    Nothing -> Literal b
+  where
+  mkln b = case directive "include" b of
+    Just s -> Include Mandatory s
+    Nothing -> case directive "optional" b of
+      Just s -> Include Optional s
+      Nothing -> Literal b
 
 findInclude :: FilePath -> [FilePath] -> IO (Maybe FilePath)
 findInclude _ [] = return Nothing
@@ -41,10 +45,10 @@ findInclude f (p:ps) = do
 
 -- This has terrible worst case complexity because unlines is strict.
 -- I suspect config files will be small enough that it wont matter.
--- Also, this will loop forever on circular dependencies
+-- XXX this will loop forever on circular dependencies
 getPreprocessedFile :: FilePath -> [FilePath]
                     -> IO (Either String B.ByteString)
-getPreprocessedFile root path = aux (Include root)
+getPreprocessedFile root path = aux (Include Mandatory root)
   where
   run f = do
     ls <- getLines f
@@ -53,10 +57,12 @@ getPreprocessedFile root path = aux (Include root)
       [] -> return (Right (B.unlines (rights bs)))
       es -> return (Left ("In file " ++ f ++ ": " ++ unlines es))
   aux (Literal b) = return (Right b)
-  aux (Include i) = do
-    found <- findInclude i path
+  aux (Include i p) = do
+    found <- findInclude p path
     case found of
       Just f' -> run f'
-      Nothing -> return (Left ("could not find include named " ++ i))
+      Nothing -> case i of
+        Mandatory -> return (Left ("could not find include named " ++ p))
+        Optional -> return (Right (B.empty))
 
 

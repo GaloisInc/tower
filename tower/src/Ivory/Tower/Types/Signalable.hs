3fdc3b711c27baa2f782e2aeb3ade9fef0e1001d
Jamey Sharp <jamey@galois.com>
2014-12-24 15:57:52 -0800

    Allow Signalable to generate init code too.
    It's somewhat a quirk of the STM32/FreeRTOS backend that signal handlers
    there just need to have the right function names. Other backends may
    need initialization steps to register signal handlers at startup.

--- tower/src/Ivory/Tower/Types/Signalable.hs
+++ tower/src/Ivory/Tower/Types/Signalable.hs
@@ -10,3 +10,7 @@ class Signalable s where
   signalName    :: s -> String
   signalHandler :: s -> (forall eff . Ivory eff ()) -> ModuleDef
 
+  -- | Code to prepare this signal for use, if necessary. The default
+  -- implementation emits no code.
+  signalInit :: s -> Ivory eff ()
+  signalInit _ = return ()

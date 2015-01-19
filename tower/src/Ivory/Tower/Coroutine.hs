e72ffc56fc672b55efabcb8e90534475b34206a0
Jamey Sharp <jamey@galois.com>
2015-01-19 15:09:09 -0800

    Coroutines: Simplify Tower wrapper.

--- tower/src/Ivory/Tower/Coroutine.hs
+++ tower/src/Ivory/Tower/Coroutine.hs
@@ -2,25 +2,23 @@
 
 module Ivory.Tower.Coroutine where
 
-import Control.Monad.Fix
+import Control.Applicative
 import Ivory.Language
 import Ivory.Tower.Handler
 import Ivory.Tower.Monad.Handler
 import Ivory.Tower.Monad.Monitor
 import Ivory.Tower.Monitor
 import Ivory.Tower.Tower
-import Ivory.Tower.Types.Chan
 
-coroutineHandler :: (IvoryArea init, IvoryZero init, IvoryArea a, IvoryZero a) => ChanOutput init -> ChanOutput a -> (forall evt. Handler evt e (Coroutine a)) -> Monitor e ()
-coroutineHandler chanInit chan block = do
-  (doInitChan, (ChanOutput (Chan readyast))) <- liftTower channel
+coroutineHandler :: (IvoryArea init, IvoryZero init, IvoryArea a, IvoryZero a) => ChanOutput init -> ChanOutput a -> String -> (forall evt. Handler evt e (CoroutineBody a)) -> Monitor e ()
+coroutineHandler chanInit chan name block = do
+  (doInitChan, readyChan) <- liftTower channel
   lastValue <- state "last_value"
 
-  name <- mfix $ \ name -> runHandler name readyast $ do
-    coro <- block
+  handler readyChan name $ do
+    coro <- coroutine <$> fmap showUnique handlerName <*> block
     liftMonitor $ monitorModuleDef $ coroutineDef coro
     callbackV $ \ shouldInit -> coroutineRun coro shouldInit $ constRef lastValue
-    return $ coroutineName coro
 
   handler chanInit (name ++ "_init") $ do
     doInit <- emitter doInitChan 1

a7f4c89ac5a3eaa11d89014851a16b423a7fe59d
leepike <leepike@gmail.com>
2015-03-04 00:13:49 -0800

    tower-aadl: major refactoring.
    Passes AADL type-checker.
    Still need to get code consistent.

--- tower-aadl/src/Tower/AADL/FromTower.hs
+++ tower-aadl/src/Tower/AADL/FromTower.hs
@@ -13,14 +13,12 @@ module Tower.AADL.FromTower
 
 import           Prelude hiding (init)
 import           System.FilePath ((</>), addExtension)
-import           Data.List (partition)
-import           Data.Monoid (mconcat)
 
 import qualified Ivory.Tower.AST as A
 import qualified Ivory.Tower.AST.Graph as G
 import qualified Ivory.Tower.Types.Time as T
 import qualified Ivory.Tower.Types.Unique as U
-import           Ivory.Tower.Codegen.Handler (callbackProcName)
+import           Ivory.Tower.Codegen.Handler (callbackProcName,handlerProcName)
 
 import           Tower.AADL.AST
 import           Tower.AADL.Config
@@ -42,73 +40,10 @@ mkProcess :: Config -> A.Tower -> Process
 mkProcess c t = Process { .. }
   where
   processName       = configSystemName c ++ "_process"
-  processComponents = activeThreads ++ passiveThreads
-  activeThreads     = map (fromThread t) (skipInitThread (A.towerThreads t))
-  passiveThreads    = map (fromMonitor c t) (A.tower_monitors t)
-  -- XXX needed with signals?
-  skipInitThread    = filter (("thread_init" /=) . A.threadName)
+  processComponents = concatMap (fromMonitor c t) (A.tower_monitors t)
 
-fromThread :: A.Tower -> A.Thread -> Thread
-fromThread twr t = Thread { .. }
-  where
-  threadName       = A.threadName t
-  threadComments   = []
-  threadFeatures   = [ChannelFeature (chanFeature twr t)]
-  threadProperties =
-    [ ThreadType Active
-    , DispatchProtocol (dispatch (A.threadChan t))
-
-    -- XXX made up for now
-    , ExecTime 10 100
-    , StackSize 100
-    , Priority 1
-    ]
-
-dispatch :: A.Chan -> DispatchProtocol
-dispatch c = case c of
-  A.ChanSignal{}  -> Aperiodic
-  A.ChanPeriod p  -> Periodic (T.toMicroseconds (A.period_dt p))
-  _               -> error $ "dispatch: " ++ show c
-
-chanFeature :: A.Tower -> A.Thread -> Channel
-chanFeature twr t = case c of
-  A.ChanSync{}
-    -> error "Impossible: in fromThread: active thread with ChanSync."
-  A.ChanSignal _s
-    -> error $ "chanFeature " ++ show c
-  A.ChanPeriod per
-    -> let chanLabel = prettyPeriod per in
-       let chanType  = A.period_ty per in
-       Channel { .. }
-  A.ChanInit _init
-    -> error $ "chanFeature " ++ show c
-  where
-  c             = A.threadChan t
-  chanHandle    = Output
-  chanCallbacks = Prim (map A.monitorName chanMonitors)
-  chanMonitors  = map fst (G.towerChanHandlers twr c)
-
-fromMonitor :: Config -> A.Tower -> A.Monitor -> Thread
-fromMonitor c t m = Thread { .. }
-  where
-  hs               = A.monitor_handlers m
-  threadComments   = catHandlerSrcLocs
-                       (concatMap A.handler_comments hs)
-  (fs, ps)         = concatPair (map (fromHandler c t) hs)
-  threadName       = A.monitorName m
-  threadFeatures   = fs
-  threadProperties = ps ++
-    [ ThreadType Passive
-    , DispatchProtocol Aperiodic
-
-    -- XXX made up for now
-    , ExecTime 10 100
-    , StackSize 100
-    , Priority 1
-    ]
-
-type Sym = String
-type Src = String
+fromMonitor :: Config -> A.Tower -> A.Monitor -> [Thread]
+fromMonitor c t m = map (fromHandler c t) (A.monitor_handlers m)
 
 -- | Create the feature groups and thread properties from a Tower handler. A
 -- handler is a collection of emitters and callbacks associated with a single
@@ -116,17 +51,55 @@ type Src = String
 fromHandler :: Config
             -> A.Tower
             -> A.Handler
-            -> ([Feature], [ThreadProperty])
-fromHandler c t h = (cf:fs, ps)
+            -> Thread
+fromHandler c t h = Thread { .. }
   where
-  (fs, ps) = concatPair (map fromEmitter (A.handler_emitters h))
-  cf       = fromInputChan cbs (A.handler_chan h)
-  cbs      = concatMap mkCbNames (A.handler_callbacks h)
+  threadName       = U.showUnique (A.handler_name h)
+  threadFeatures   = cf:fs
+  threadComments   = A.handler_comments h
+  (fs, ps)         = concatPair (map fromEmitter (A.handler_emitters h))
+  cf               = fromInputChan cbs (A.handler_chan h)
+  cbs              = concatMap mkCbNames (A.handler_callbacks h)
+  (threadType, dispatch)
+    = case A.handler_chan h of
+        A.ChanSignal sig -- XXX address is a lie
+            -> (Active, Signal (A.signal_name sig) 0xdeadbeef)
+        A.ChanPeriod per
+            -> ( Active
+               , Periodic (T.toMicroseconds (A.period_dt per))
+               )
+        A.ChanInit{} -- XXX is Aperiodic right? We're ignoring init
+                     -- chans for now, anyways
+            -> (Active, Aperiodic)
+        A.ChanSync{}
+            -> (Passive, Aperiodic)
+  threadPriority   = case threadType of
+                       Active  -> [Priority 1] -- XXX made up for now
+                       Passive -> []
+  stackSize        = case threadType of
+                       Active  -> [StackSize 100] -- XXX made up for now
+                       Passive -> []
+  propertySrcText  =
+    case threadType of
+      Active
+        -> [PropertySourceText ("foobar", handlerProcName h t')]
+           where
+           t' = case A.handler_chan h of
+                  A.ChanSignal sig -> A.SignalThread sig
+                  A.ChanPeriod per -> A.PeriodThread per
+                  A.ChanInit i     -> A.InitThread   i
+
+      Passive -> []
+  threadProperties =
+      ThreadType threadType
+    : DispatchProtocol dispatch
+    : ExecTime 10 100 -- XXX made up for now
+    : propertySrcText ++ stackSize ++ threadPriority ++ ps
 
   -- Use Tower's API to create a callback function symbol based on the callback
   -- (a `Unique`) and the Tower threads that call it. Those functions are
   -- defined in .c files named after their active threads.
-  mkCbNames :: U.Unique -> [(Src, Sym)]
+  mkCbNames :: U.Unique -> [SourcePath]
   mkCbNames cb = map (mkSrc &&& mkSym) cbThds
     where
     mkSrc thd = (flip addExtension) "c"
@@ -135,13 +108,13 @@ fromHandler c t h = (cf:fs, ps)
     mkSym     = callbackProcName cb (A.handler_name h)
     cbThds    = G.handlerThreads t h
 
-fromInputChan :: [(Src, Sym)] -> A.Chan -> Feature
+fromInputChan :: [SourcePath] -> A.Chan -> Feature
 fromInputChan callbacks c = case c of
   A.ChanSync s
     -> let chanType  = A.sync_chan_type  s in
        let chanLabel = syncChanLabel s in
        ChannelFeature (Channel { .. })
-  A.ChanSignal sig
+  A.ChanSignal{}
     -> error $ "fromInputChan " ++ show c
   A.ChanPeriod per
     -> let chanLabel = prettyPeriod per in
@@ -174,10 +147,10 @@ emitterChan proc c = case c of
   _ -> error $ "Impossible channel " ++ show c ++ " in emitterChan."
   where
   chanHandle    = Output
-  chanCallbacks = Prim [proc]
+  chanCallbacks = Prim proc
 
 syncChanLabel :: A.SyncChan -> String
-syncChanLabel = ("sync_" ++) . show . A.sync_chan_label
+syncChanLabel = show . A.sync_chan_label
 
 --------------------------------------------------------------------------------
 -- Helpers
@@ -187,11 +160,3 @@ concatPair = (concat *** concat) . unzip
 
 prettyPeriod :: A.Period -> String
 prettyPeriod = T.prettyTime . A.period_dt
-
--- concatenate source locations, then return all comments.
-catHandlerSrcLocs :: [A.Comment] -> [A.Comment]
-catHandlerSrcLocs cs = srcloc : strs
-  where
-  (strs, srclocs) =
-    partition (\case {A.UserComment{} -> True; A.SourcePos{} -> False}) cs
-  srcloc = A.SourcePos (mconcat (map (\(A.SourcePos sl) -> sl) srclocs))

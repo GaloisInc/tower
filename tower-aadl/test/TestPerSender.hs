824aaad6f76490424f661db03610af838b6b3c3a
Lee Pike <leepike@galois.com>
2015-03-12 15:42:56 -0400

    tower-aadl: periodic send test.

---
+++ tower-aadl/test/TestPerSender.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE PostfixOperators #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+--------------------------------------------------------------------------------
+-- Call from a periodic thread.
+--
+-- (c) 2015 Galois, Inc.
+--
+--------------------------------------------------------------------------------
+
+module Main where
+
+import Ivory.Tower
+import Ivory.Language
+import Tower.AADL
+
+test0 :: Tower e ()
+test0 = do
+  towerModule  towerDepModule
+  towerDepends towerDepModule
+
+  (cin, _) <- channel
+  per <- period (100`ms`)
+
+  monitor "sender" $ do
+    handler per "tick" $ do
+      e <- emitter cin 1
+      callback $ \msg -> do
+        m <- deref msg
+        call_ printf "Sender ping received %d. Writing to receiver.\n" m
+        emitV e (m+1)
+
+--------------------------------------------------------------------------------
+-- Compiler
+
+main :: IO ()
+main =
+  runCompileAADL
+    initialOpts { genDirOpts = Just "test0"
+                , configOpts = config
+                }
+    test0
+  where
+  config = initialConfig { configSrcsDir = "test0/user_code" }
+
+--------------------------------------------------------------------------------
+-- Helpers
+
+printf :: Def('[IString, ITime] :-> ())
+printf = importProc "printf" "stdio.h"
+
+towerDepModule :: Module
+towerDepModule = package "towerDeps" (incl printf)

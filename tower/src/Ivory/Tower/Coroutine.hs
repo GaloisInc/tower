2c5cc1d281585d6284205345a345923544e4bacd
leepike <leepike@gmail.com>
2015-03-11 17:34:28 -0700

    tower: comments for coroutine handlers.

--- tower/src/Ivory/Tower/Coroutine.hs
+++ tower/src/Ivory/Tower/Coroutine.hs
@@ -10,7 +10,26 @@ import Ivory.Tower.Monad.Monitor
 import Ivory.Tower.Monitor
 import Ivory.Tower.Tower
 
-coroutineHandler :: (IvoryArea init, IvoryZero init, IvoryArea a, IvoryZero a) => ChanOutput init -> ChanOutput a -> String -> (forall evt. Handler evt e (CoroutineBody a)) -> Monitor e ()
+
+-- | Lifts an Ivory coroutine into a set of Tower handlers. Takes the sink of
+-- two channels: (1) whether the coroutine should be initialized @chanInit@, and
+-- a (2) value to process over the @chan@ channel. The value of @chanInit@ is
+-- ignored; whenever a message is received over the channel, the coroutine is
+-- reinitialized. Otherwise, values from the @chan@ channel are passed to the
+-- coroutine. Example usage:
+--
+-- @
+--     coroutineHandler initChan resChan "foo" $ do
+--       e <- emitter chan 1
+--       return $ CoroutineBody $ \yield -> do
+--         ... Ivory code ...
+-- @
+coroutineHandler :: (IvoryArea init, IvoryZero init, IvoryArea a, IvoryZero a)
+                 => ChanOutput init
+                 -> ChanOutput a
+                 -> String
+                 -> (forall evt. Handler evt e (CoroutineBody a))
+                 -> Monitor e ()
 coroutineHandler chanInit chan name block = do
   (doInitChan, readyChan) <- liftTower channel
   lastValue <- state "last_value"

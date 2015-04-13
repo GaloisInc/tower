4ca87a73dcb0157dc3cffecb7ca550784fbf0e65
leepike <leepike@gmail.com>
2015-04-13 12:06:09 -0700

    tower-aadl: generate build artifacts.

--- tower-aadl/src/Tower/AADL/Build.hs
+++ tower-aadl/src/Tower/AADL/Build.hs
@@ -5,18 +5,39 @@
 
 module Tower.AADL.Build where
 
+import Tower.AADL.Config (Config(..))
+
 buildScript :: String
 buildScript = unlines
   [
     "#!/bin/sh"
   , ""
   , "# Source AADL_FILES"
-  , ". $AADL_FILES"
+  , ". AADL_FILES"
   , ""
-  , "RAMSES=$PATH_TO_RAMSES/ramses-demo"
+  , "# Source Ramses path"
+  , ". ../RAMSES_PATH"
   , ""
-  , "export RAMSES_DIR=$RAMSES/ramses_resource"
-  , "export AADL2RTOS_CONFIG_DIR=$RAMSES/aadl2rtos_resource"
+  , "export RAMSES_DIR=$RAMSES_PATH/ramses_resource"
+  , "export AADL2RTOS_CONFIG_DIR=$RAMSES_PATH/aadl2rtos_resource"
   , ""
-  , "java -jar $RAMSES/ramses.jar -g rtos -i $AADL2RTOS_CONFIG_DIR -o . -l trace -s sys.impl -m SMACCM_SYS.aadl,$AADL_LIST"
+  , "java -jar $RAMSES_PATH/ramses.jar -g rtos -i $AADL2RTOS_CONFIG_DIR -o . -l trace -s sys.impl -m SMACCM_SYS.aadl,$AADL_LIST"
   ]
+
+makefile :: Config -> String
+makefile c = unlines
+  [
+    ".PHONY: ramses"
+  , "ramses:"
+  , tab "sh build.sh"
+  , ""
+  , ".PHONY: tower-clean"
+  , "tower-clean:"
+  , tab "-rm build.sh"
+  , tab "-rm AADL_FILES"
+  , tab "-rm Makefile"
+  , tab "-rm *.aadl"
+  , tab "-rm -rf " ++ configSrcsDir c
+  , tab "-rm -rf " ++ configHdrDir  c
+  ]
+  where tab = ('\t' :)

af4e4952cc6904f3ac0d21e9c0576c6274fe1c46
leepike <leepike@gmail.com>
2015-04-15 11:09:45 -0700

    tower-aadl: generate kconfig, kbuild, etc.
    Also decompose Makefiles.

--- tower-aadl/src/Tower/AADL/Build.hs
+++ tower-aadl/src/Tower/AADL/Build.hs
@@ -5,7 +5,11 @@
 
 module Tower.AADL.Build where
 
+import Data.Char
 import Tower.AADL.Config (Config(..))
+
+--------------------------------------------------------------------------------
+-- Ramses build
 
 buildScript :: String
 buildScript = unlines
@@ -24,12 +28,15 @@ buildScript = unlines
   , "java -jar $RAMSES_PATH/ramses.jar -g rtos -i $AADL2RTOS_CONFIG_DIR -o . -l trace -s sys.impl -m SMACCM_SYS.aadl,$AADL_LIST"
   ]
 
-makefile :: Config -> String
-makefile c = unlines
+buildScriptName :: String
+buildScriptName = "ramses-build.sh"
+
+ramsesMakefile :: Config -> String
+ramsesMakefile c = unlines
   [
     ".PHONY: ramses"
   , "ramses:"
-  , tab "sh build.sh"
+  , tab "sh " ++ buildScriptName
   , ""
   , ".PHONY: tower-clean"
   , "tower-clean:"
@@ -40,4 +47,63 @@ makefile c = unlines
   , tab "-rm -rf " ++ configSrcsDir c
   , tab "-rm -rf " ++ configHdrDir  c
   ]
-  where tab = ('\t' :)
+
+ramsesMakefileName :: String
+ramsesMakefileName = "ramses.mk"
+
+--------------------------------------------------------------------------------
+-- Kbuild, Kconfig
+
+kbuild :: String -> String
+kbuild dir = unlines
+  [ "apps-$(CONFIG_APP_" ++ shellVar dir ++ ") += " ++ dir
+  , dir ++ ": libsel4 libmuslc libsel4platsupport libsel4muslccamkes libsel4camkes libsel4sync libsel4debug libsel4bench"
+  ]
+
+kbuildName :: String
+kbuildName = "Kbuild"
+
+kconfig :: String -> String -> String
+kconfig prog dir = unlines
+  [ "config APP_" ++ shellVar dir
+  , "    bool \"Generated code for " ++ prog ++ " .\""
+  , "    default n"
+  , "    select APP_CAMKES_EXECUTIVE"
+  , "    help"
+  , "        Generated AADL from " ++ prog
+  ]
+
+kconfigName :: String
+kconfigName = "Kconfig"
+
+camkesMakefileName :: String
+camkesMakefileName = "camkesmakefile.mk"
+
+otherCamkesTargets :: String
+otherCamkesTargets = "othercamkestargets.mk"
+
+makefile :: String
+makefile = unlines
+  [
+    incl ramsesMakefileName
+  , incl camkesMakefileName
+  , incl otherCamkesTargets
+  , ""
+  , "camkesMakefileName: " ++ mkTp
+  , tab $ unwords ["cp ", mkTp, camkesMakefileName]
+  ]
+  where
+  mkTp = "make_template/Makefile"
+  incl = ("-include " ++)
+
+makefileName :: String
+makefileName = "Makefile"
+
+--------------------------------------------------------------------------------
+-- Helpers
+
+tab :: String -> String
+tab = ('\t' :)
+
+shellVar :: String -> String
+shellVar = map toUpper

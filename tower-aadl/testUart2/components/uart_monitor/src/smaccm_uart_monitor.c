c7980981e418b6c6725692d84177701d09486e4d
leepike <leepike@gmail.com>
2015-05-06 12:27:10 -0700

    tower-aadl: deprecate test apps.

--- tower-aadl/testUart2/components/uart_monitor/src/smaccm_uart_monitor.c
+++
@@ -1,9 +0,0 @@
-#include <uart_monitor.h>
-
-bool Input0_write_Data_Types__uart_packet_impl(const Data_Types__uart_packet_impl * input) {
-  int r = uart_write(input->uart_num, input->datum);
-  if (r < 0) {
-    printf("Error from uart_write, return code: %d\n", r);
-  }
-  return (r >= 0);
-}

0c85264c63fd139b1a998e64f314e2e22d11394e
Pat Hickey <pat@galois.com>
2015-05-11 19:09:32 -0700

    tower: totally new tower compiler api
    gives more responsibilities to the individual backends.
    
    we ended up needing to generalize this in order to get the tower
    AADL backend to work. We weren't getting any mileage out of the TowerPlatform
    abstraction, it really only worked for the freertos backend.

--- tower/src/Ivory/Tower/Compile.hs
+++ tower/src/Ivory/Tower/Compile.hs
@@ -1,35 +1,17 @@
 module Ivory.Tower.Compile
   ( towerCompile
-  , runTowerCompile
   , TOpts(..)
   ) where
 
 import qualified Ivory.Compile.C.CmdlineFrontend as C
-import Ivory.Tower.Backend
+import Ivory.Language
+import Ivory.Artifact
 import Ivory.Tower.Compile.Options
-import Ivory.Tower.Tower
-import Ivory.Tower.Types.Dependencies
-import Ivory.Tower.Types.TowerPlatform
 import System.Environment (getArgs)
 
-towerCompile :: TowerBackend backend
-             => (TOpts -> IO (TowerPlatform backend e))
-             -> Tower e ()
-             -> IO ()
-towerCompile mkPlatform t = do
+towerCompile :: IO (TOpts, [Module] -> [Artifact] -> IO ())
+towerCompile = do
   args <- getArgs
   (copts, topts) <- getOpts args
-  p <- mkPlatform topts
-  runTowerCompile t p copts
+  return (topts, \mods as -> C.runCompilerWith Nothing mods as copts)
 
-runTowerCompile :: TowerBackend backend
-                => Tower e ()
-                -> TowerPlatform backend e
-                -> C.Opts
-                -> IO ()
-runTowerCompile t p opts = C.runCompilerWith Nothing mods as opts
-  where
-  (_ast, output, deps, sigs) = runTower (platformBackend p) t (platformEnv p)
-  mods = dependencies_modules deps ++ addModules p output deps sigs
-  givenArtifacts = dependencies_artifacts deps
-  as = addArtifacts p output deps sigs mods givenArtifacts ++ givenArtifacts

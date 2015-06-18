0be87fc96e8579098bf65e3a780987ef4f1d8eff
leepike <leepike@gmail.com>
2015-06-17 22:09:06 -0700

    tower-aadl: delete old testing files.

--- tower-aadl/testUart/setup.sh
+++
@@ -1,5 +0,0 @@
-mkdir components
-cp -r /Users/leepike/vm-shared/testUart-works/components/uart_driver components
-make ramses
-cp /Users/leepike/vm-shared/testUart-works/components/uart_monitor_11/uart_monitor_11.camkes ./components/uart_monitor_11
-cp /Users/leepike/vm-shared/testUart-works/othercamkestargets.mk ./
